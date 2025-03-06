024d03c9381bf1f13752f5b6c32ced488b29450d
Mihai Valcu <valcu@orn.mpg.de>
2025-03-06 17:57:20 +0100

    -

--- inst/UI/global.R
+++ inst/UI/global.R
@@ -31,9 +31,7 @@
 
 #* Variables
   SERVER = "localhost"
-  SERVER = "de_fieldworker"
   DB = "tests"
-  DB = "FIELD_2025_NOLAatDUMMERSEE"
   GPS_IDS = 1:10
   EXPORT_TABLES = c("mid_points", "mid_tracks")
   
